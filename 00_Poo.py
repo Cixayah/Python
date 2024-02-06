6002203296ff154190b92b6c74be2860c580cd72
Cix <gabrielviniciodacosta@gmail.com>
2024-02-06 15:14:31 -0300

    #

--- 00_Poo.py
+++ 00_Poo.py
@@ -1,18 +1,18 @@
-#Poo
-#Classe de exemplo
+# Poo
+# Classe de exemplo
 class People: 
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
     
     def hello(self):
-        return f"Hello, my name is {self.name} and i have {self.age} years old."
+        return f"Hello, my name is {self.name} and I have {self.age} years old."
 
-#Objetos
-pOne = People("Larissa", 23)
-hello = pOne.hello()
-print(hello)
+# Objetos
+people_one = People("Larissa", 23)
+hello_one = people_one.hello()
+print(hello_one)
 
-pTwo = People("Gabriel", 26)
-hello = pTwo.hello()
-print(hello)
+people_two = People("Gabriel", 26)
+hello_two = people_two.hello()
+print(hello_two)
--- 01_Pilares_Poo.py
+++ 01_Pilares_Poo.py
@@ -1,32 +1,52 @@
-#Exemplo de Herança
-    # \n significa quebra de linha
+# Exemplo de herança
+# \n significa quebra de linha
 print("\nExemplo de herança:")
+
 class Animal:
     def __init__(self, name) -> None:
         self.name = name
-        
+
     def walk(self):
-        print("The animal {self.name} walked. ")
+        print(f"The animal {self.name} walked.")
         return
-    
-    def animalSound(self):
+
+    def animal_sound(self):
         pass
+
 class Dog(Animal):
-    def animalSound(self):
+    def animal_sound(self):
         return "Au, au"
-    
+
 class Cat(Animal):
-    def animalSound(self):
+    def animal_sound(self):
         return "Miau!"
 
-
-dog = Dog("Nick") #Poderia colocar nome="Nick" mas assim é melhor (poupa código).
+dog = Dog("Nick")  # Poderia colocar nome="Nick" mas assim é melhor (poupa código).
 cat = Cat("Larissinha")
 
-print("\nExemplo de polomorfismo")
+print("\nExemplo de polimorfismo")
 animals = [dog, cat]
 
 for animal in animals:
-    print(f"{animal.name} faz: {animal.animalSound()}")
+    print(f"{animal.name} faz: {animal.animal_sound()}")
 
 print("\nExemplo de encapsulamento")
+
+class BankAccount:
+    def __init__(self, balance) -> None:
+        self.__balance = balance  # Atributo privado __balance
+
+    def deposit(self, value):
+        if value > 0:
+            self.__balance += value
+
+    def withdraw(self, value):
+        if value > 0 and value <= self.__balance:
+            self.__balance -= value
+
+    def check_balance(self):
+        return self.__balance
+
+bank_account = BankAccount(balance=1000)
+print(f"Saldo da conta bancária: {bank_account.check_balance()}")
+bank_account.deposit(value=500)  # Corrigi a chamada do método deposit
