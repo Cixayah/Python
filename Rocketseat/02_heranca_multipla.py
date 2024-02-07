7bdcfc81e46108d1d9f2f94a74118cb24773a481
Cix <gabrielviniciodacosta@gmail.com>
2024-02-07 15:48:09 -0300

    #

---
+++ Rocketseat/02_heranca_multipla.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self) -> None:
+        self.name = name
+        
+    def animal_sound(self):
+        pass
+    
+class Carnivorous(Animal):
+    def eat_meat(self):
+        return f"{self.name}"
